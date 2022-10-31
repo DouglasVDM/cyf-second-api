8a3abb484e45e4997edaa18901f36667f1f0341a
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2022-10-31 20:54:54 +0200

    change port #

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const express = require('express'); 
-const app = express();             
-const port = 5001;                  
+const express = require('express');
+const app = express();
+const port = 5002;
 
 let YOUR_WORST_FOOD = "fast food"
 let YOUR_NAME = "douglas"
 
-app.get('/', (req, res) => {       
-    res.send(`Welcome to ${YOUR_NAME}'s 2nd API`);     
-                                                     
+app.get('/', (req, res) => {
+    res.send(`Welcome to ${YOUR_NAME}'s 2nd API`);
+
 });
 
-app.get('/worstfood', (req, res) => {       
-    res.send(`My worst food is ${YOUR_WORST_FOOD}`);                                                       
+app.get('/worstfood', (req, res) => {
+    res.send(`My worst food is ${YOUR_WORST_FOOD}`);
 });
 
-app.get('/worstfoodfrom2', (req, res) => {       
-    res.send(YOUR_WORST_FOOD);                                                       
+app.get('/worstfoodfrom2', (req, res) => {
+    res.send(YOUR_WORST_FOOD);
 });
 
-app.listen(port, () => {          
-    console.log(`Now listening on port ${port}`); 
+app.listen(port, () => {
+    console.log(`Now listening on port ${port}`);
 });
